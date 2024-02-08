c2d3c9a2d8dca5511c07e07941fc8a7945b81980
jharasaki <156235690+jharasaki@users.noreply.github.com>
2024-02-08 11:43:15 -0800

    vs

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,29 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
+  @Test
+  public void testReverseInPlace() {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
-	}
-
+    assertArrayEquals(new int[] { 3 }, input1);
+  }
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {};
+    assertArrayEquals(new int[] {}, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] input2 = { 0, 2, 4 };
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[] { 4, 2, 0 }, input2);
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input2 = { 0, 2, 4 };
+    assertArrayEquals(new int[] { 4, 2, 0 }, ArrayExamples.reversed(input2));
   }
 }
